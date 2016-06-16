46e445b5a5586d9ba6b4a348d77cf87ca9dd9469
msdx321 <msdx321@gmail.com>
2016-06-16 10:24:55 +0800

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-export PATH=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin:$PATH 
+export PATH=/home/bit/Android/CyanogenMod/cm-13.0/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin:$PATH
+export CROSS_COMPILE=/home/bit/Android/CyanogenMod/cm-13.0/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export ARCH=arm64
 export SEC_BUILD_OPTION_HW_REVISION=02
 
-mkdir out
+rm -rf ~/out
+mkdir ~/out
 
+<<<<<<< HEAD
 make -C $(pwd) O=$(pwd)/out ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- KCFLAGS=-mno-android hero2qlte_chnzc_defconfig
 make -C $(pwd) O=$(pwd)/out ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- KCFLAGS=-mno-android
 
 cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
+=======
+make -C $(pwd) O=/home/bit/out KCFLAGS=-mno-android msdx321_defconfig
+make -C $(pwd) O=/home/bit/out KCFLAGS=-mno-android -j8
+>>>>>>> 8da1926... Update build_kernel.sh
