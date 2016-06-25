55a600686c76163ecb0733aa28fdb1289527cead
msdx321 <msdx321@gmail.com>
2016-06-25 22:15:25 +0800

    Add ramdisk

--- build_kernel.sh
+++ build_kernel.sh
@@ -10,5 +10,9 @@ make -C $(pwd) O=$(pwd)/out KCFLAGS=-mno-android -j8
 
 $(pwd)/toolchains/dtbTool/dtbToolCM -v -s 2048 -o out/arch/arm64/boot/dt.img  out/arch/arm64/boot/dts/samsung/
 
-cp out/arch/arm64/boot/dt.img out/dt_image
-cp out/arch/arm64/boot/Image.gz out/kernel.gz
+cp -f out/arch/arm64/boot/dt.img prebuilt-boot/boot.img-dt
+cp -f out/arch/arm64/boot/Image.gz prebuilt-boot/boot.img-zImage
+
+cd prebuilt-boot
+rm -rf boot.img
+$(pwd)/mkboot/mkbootimg --kernel boot.img-zImage --ramdisk boot.img-ramdisk.gz --cmdline "console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=24M@0-0xffffffff rcupdate.rcu_expedited=1" --tags_offset 02000000 --dt boot.img-dt --base 80000000 --pagesize 4096 -o boot.img
