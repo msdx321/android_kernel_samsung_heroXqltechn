9255f1c87a7be2096f6298a0f5d58ca66e197555
msdx321 <msdx321@gmail.com>
2016-06-27 17:20:46 +0800

    fix derp[WIP]

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -164,7 +164,6 @@ int suspend_valid_only_mem(suspend_state_t state)
 }
 EXPORT_SYMBOL_GPL(suspend_valid_only_mem);
 
-<<<<<<< HEAD
 static bool sleep_state_supported(suspend_state_t state)
 {
 	return state == PM_SUSPEND_FREEZE || (suspend_ops && suspend_ops->enter);
@@ -234,28 +233,6 @@ static bool platform_suspend_again(suspend_state_t state)
 {
 	return state != PM_SUSPEND_FREEZE && suspend_ops->suspend_again ?
 		suspend_ops->suspend_again() : false;
-=======
-static bool platform_suspend_again(void)
-{
-	int count;
-	bool suspend = suspend_ops->suspend_again ?
-		suspend_ops->suspend_again() : false;
-
-	if (suspend) {
-		/*
-		 * pm_get_wakeup_count() gets an updated count of wakeup events
-		 * that have occured and will return false (i.e. abort suspend)
-		 * if a wakeup event has been started during suspend_again() and
-		 * is still active. pm_save_wakeup_count() stores the count
-		 * and enables pm_wakeup_pending() to properly analyze wakeup
-		 * events before entering suspend in suspend_enter().
-		 */
-		suspend = pm_get_wakeup_count(&count, false) &&
-			  pm_save_wakeup_count(count);
-	}
-
-	return suspend;
->>>>>>> 66a472c... Enable QuickWakeUp feature
 }
 
 static int suspend_test(int level)
@@ -453,12 +430,7 @@ int suspend_devices_and_enter(suspend_state_t state)
 
 	do {
 		error = suspend_enter(state, &wakeup);
-<<<<<<< HEAD
 	} while (!error && !wakeup && platform_suspend_again(state));
-=======
-	} while (!error && !wakeup && need_suspend_ops(state)
-		&& platform_suspend_again());
->>>>>>> 66a472c... Enable QuickWakeUp feature
 
  Resume_devices:
 	suspend_test_start();
