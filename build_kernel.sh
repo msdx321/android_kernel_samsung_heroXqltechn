1b6db0942aac4cf776a119a5372e89921f4e877f
msdx321 <msdx321@gmail.com>
2016-06-27 19:02:46 +0800

    Use Anykernel2 to pack

--- build_kernel.sh
+++ build_kernel.sh
@@ -10,9 +10,8 @@ make -C $(pwd) O=$(pwd)/out KCFLAGS=-mno-android -j8
 
 $(pwd)/toolchains/dtbTool/dtbToolCM -v -s 2048 -o out/arch/arm64/boot/dt.img  out/arch/arm64/boot/dts/samsung/
 
-cp -f out/arch/arm64/boot/dt.img prebuilt-boot/boot.img-dt
-cp -f out/arch/arm64/boot/Image.gz prebuilt-boot/boot.img-zImage
+cp -f out/arch/arm64/boot/dt.img prebuilt-anykernel/dtb
+cp -f out/arch/arm64/boot/Image.gz prebuilt-anykernel/zImage
 
-cd prebuilt-boot
-rm -rf boot.img
-$(pwd)/mkboot/mkbootimg --kernel boot.img-zImage --ramdisk boot.img-ramdisk.gz --cmdline "console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=24M@0-0xffffffff rcupdate.rcu_expedited=1" --tags_offset 02000000 --dt boot.img-dt --base 80000000 --pagesize 4096 -o boot.img
+cd prebuilt-anykernel
+zip -r BKernel.zip ./*
